be5c35cccbd9f51a3cc75650bb11b55fc4fbeb68
Yevhenii <68529563+YevheniiRudenko@users.noreply.github.com>
2022-07-05 12:30:14 +0300

    Create GenerateHash.ps1

---
+++ .github/scripts/GenerateHash.ps1
@@ -0,0 +1,27 @@
+<#
+.Synopsis
+   Generate hash from string
+.DESCRIPTION
+   Generates a short hash (5 first characters) from the part of the string that comes after the last forward slash (/)
+.EXAMPLE
+   Given "features/SIL-1" will use "SIL-1" as input for the hash function and create a 5 character output from that
+#>
+
+param(
+    [Parameter(Mandatory)]
+    [string]$stringToHash
+)
+
+# Take the last part of the string
+$lastPartOfString =$stringToHash.Split("/")[-1]
+
+# Calculate hash
+$hash=[System.Security.Cryptography.HashAlgorithm]::Create("sha256").ComputeHash(
+[System.Text.Encoding]::UTF8.GetBytes($lastPartOfString))
+
+# Convert to hex
+$longhash=[System.BitConverter]::ToString($hash).Replace("-","").ToLower()
+
+# Create short hash
+$shorthash=$longhash.SubString(0,5)
+Write-Output $shorthash
